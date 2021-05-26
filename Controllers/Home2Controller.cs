5ad1c9d482a1ffe97fe1624b4767a66eb87d20d6
Davi <davicustodioburdinski@gmail.com>
2021-05-26 00:09:56 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/Home2Controller.cs
@@ -0,0 +1,42 @@
+﻿using formulario2.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace formulario2.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        // GET: Home2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult formularios()
+        {
+            return View();
+        }
+
+        public JsonResult getForm(string pergunta1, string pergunta2, string pergunta3, string nome )
+        {
+            List<Data> list = new List<Data>();
+
+            Data data = new Data();
+
+            data.pergunta1 = pergunta1;
+            data.pergunta2 = pergunta2;
+            data.pergunta3 = pergunta3;
+            data.nome = nome;
+
+            list.Add(data);
+
+
+            return Json(list);
+        }
+         
+    }
+}
---
+++ Models/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace formulario2.Models
+{
+    public class Data
+    {
+        public string pergunta1 { get; set; }
+        public string pergunta2 { get; set; }
+        public string pergunta3 { get; set; }
+        public string nome { get; set; }
+    }
+}
